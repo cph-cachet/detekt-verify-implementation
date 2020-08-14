4ac14b3af0e7994aba41b37282a86d9777d428d9
Steven Jeuris <steven.jeuris@gmail.com>
2020-08-14 18:00:31 +0200

    Add `DataClass` and `Immutable` rules to rule provider.

--- src/main/kotlin/dk/cachet/detekt/extensions/VerifyImplementationRuleSetProvider.kt
+++ src/main/kotlin/dk/cachet/detekt/extensions/VerifyImplementationRuleSetProvider.kt
@@ -1,5 +1,7 @@
 package dk.cachet.detekt.extensions
 
+import dk.cachet.detekt.extensions.rules.DataClass
+import dk.cachet.detekt.extensions.rules.Immutable
 import io.gitlab.arturbosch.detekt.api.Config
 import io.gitlab.arturbosch.detekt.api.RuleSet
 import io.gitlab.arturbosch.detekt.api.RuleSetProvider
@@ -10,5 +12,11 @@ class VerifyImplementationRuleSetProvider : RuleSetProvider
 	override val ruleSetId: String = "verify-implementation"
 	
 	
-	override fun instance( config: Config ): RuleSet = RuleSet( ruleSetId, emptyList() )
+	override fun instance( config: Config ): RuleSet = RuleSet(
+		ruleSetId,
+		listOf(
+			DataClass( config ),
+			Immutable( config )
+		)
+	)
 }
